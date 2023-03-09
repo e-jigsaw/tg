b52fd020e55a9f7b3b7bd1a4d69a8240a7a562ac
jigsaw <m@jgs.me>
2023-03-09 14:04:11 +0000

    --write

--- index.ts
+++ index.ts
@@ -1,20 +1,23 @@
-import {Telegraf} from 'telegraf'
-import {message} from 'telegraf/filters'
+import { Telegraf } from "telegraf";
+import { message } from "telegraf/filters";
 
-const bot = new Telegraf(process.env.TOKEN!)
+const bot = new Telegraf(process.env.TOKEN!);
 
-const PROD_ROOM = parseInt(process.env.PROD_ROOM!)
-const DEV_ROOM = parseInt(process.env.DEV_ROOM!)
+const PROD_ROOM = parseInt(process.env.PROD_ROOM!);
+const DEV_ROOM = parseInt(process.env.DEV_ROOM!);
 
-bot.on(message('text'), ctx => {
-  if (ctx.update.message.chat.id === PROD_ROOM || ctx.update.message.chat.id === DEV_ROOM) {
-    console.log(ctx.update)
+bot.on(message("text"), (ctx) => {
+  if (
+    ctx.update.message.chat.id === PROD_ROOM ||
+    ctx.update.message.chat.id === DEV_ROOM
+  ) {
+    console.log(ctx.update);
   }
-})
+});
 
 bot.launch({
   webhook: {
     domain: process.env.DOMAIN!,
-    port: 3000
-  }
-})
+    port: 3000,
+  },
+});
